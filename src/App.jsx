2b90beefcde1fa72af51a83dde30283dab276673
Jeff Ouda <jeffoudahabakuk@gmail.com>
2026-02-06 23:22:31 +0300

    refactor: update App.jsx with nested routes for buyer dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,13 @@ import BrowseLivestock from './pages/BrowseLivestock'
 import Marketplace from './pages/Marketplace'
 import LivestockDetail from './pages/LivestockDetail'
 import BuyerDashboard from './pages/BuyerDashboard'
+import BuyerOverview from './pages/BuyerOverview'
+import BuyerOrders from './pages/BuyerOrders'
+import BuyerWishlist from './pages/BuyerWishlist'
+import BuyerSettings from './pages/BuyerSettings'
 import FarmerDashboard from './pages/FarmerDashboard'
+import Unauthorized from './pages/Unauthorized'
+import ProtectedRoute from './components/ProtectedRoute'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
@@ -14,13 +20,32 @@ function App() {
     <>
       <Navbar />
       <Routes>
+        {/* Public Routes */}
         <Route path="/" element={<Home />} />
         <Route path="/auth" element={<SignUp />} />
         <Route path="/browse" element={<BrowseLivestock />} />
         <Route path="/marketplace" element={<Marketplace />} />
         <Route path="/livestock/:id" element={<LivestockDetail />} />
-        <Route path="/dashboard" element={<BuyerDashboard />} />
-        <Route path="/farmer-dashboard" element={<FarmerDashboard />} />
+        <Route path="/unauthorized" element={<Unauthorized />} />
+
+        {/* Protected Buyer Routes */}
+        <Route path="/dashboard" element={
+          <ProtectedRoute allowedRoles={['buyer']}>
+            <BuyerDashboard />
+          </ProtectedRoute>
+        }>
+          <Route index element={<BuyerOverview />} />
+          <Route path="orders" element={<BuyerOrders />} />
+          <Route path="wishlist" element={<BuyerWishlist />} />
+          <Route path="settings" element={<BuyerSettings />} />
+        </Route>
+
+        {/* Protected Farmer Routes */}
+        <Route path="/farmer-dashboard" element={
+          <ProtectedRoute allowedRoles={['farmer']}>
+            <FarmerDashboard />
+          </ProtectedRoute>
+        } />
       </Routes>
       <Footer />
     </>
