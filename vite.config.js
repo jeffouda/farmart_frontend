8c2a6ea58c80839018e407cbe1fd5600060bec27
Enock Cheruiyot <cheruiyote518@gmail.com>
2026-02-06 11:04:13 +0300

    adds browser router to main.jsx

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite"; // Import the plugin
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  plugins: [
+    react(),
+    tailwindcss(), // Add the plugin here
+  ],
 });
