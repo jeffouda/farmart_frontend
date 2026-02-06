9834dc5366a730aad8baef261b8ceb6e68c1b905
Enock Cheruiyot <cheruiyote518@gmail.com>
2026-02-06 10:32:32 +0300

    works on home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,14 +6,12 @@ import Footer from "../components/Footer";
 
 function Home() {
   return (
-    // Replaced inline styles with Tailwind for consistency with your other components
     <div className="min-h-screen w-full bg-white overflow-x-hidden">
       <Navbar />
 
       <main>
         <Header />
 
-        {/* Added an ID here to ensure the Navbar "About Us" link lands correctly */}
         <div id="about">
           <About />
         </div>
