ab7dc3a420b4f75eaa46e47ee1ccd711a3310785
Esther nekesa <esthernekesasi32@gmail.com>
2026-02-11 08:39:57 +0300

    initialize wishlist component with redux state and helpers

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -8,3 +8,17 @@ import {
   optimisticRemoveFromWishlist,
 } from "../redux/wishlistSlice";
 import toast from "react-hot-toast";
+const Wishlist = () => {
+  const dispatch = useDispatch();
+  const wishlistItems = useSelector(state => state.wishlist.items);
+
+  const formatPrice = (price) => {
+    return KSh ${price.toLocaleString()};
+  };
+
+  // Helper to get animal data (handles both API and local structure)
+  const getAnimalData = (item) => {
+    return item.animal || item;
+  };
+
+
